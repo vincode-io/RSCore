0325bb8358adbace8e4e37e3674bb5d5c5f85767
Maurice Parker <mo@vincode.io>
2023-03-20 15:26:47 -0500

    Added the CloudKitValue property wrapper

--- Sources/RSCore/CloudKit/CloudKitModel.swift
+++ Sources/RSCore/CloudKit/CloudKitModel.swift
@@ -8,87 +8,52 @@
 import Foundation
 import CloudKit
 
-// https://www.swiftbysundell.com/articles/accessing-a-swift-property-wrappers-enclosing-instance/#proxy-properties
-//@propertyWrapper
-//public struct CloudKitValue<EnclosingType, Value: Equatable> where Value: Codable {
-//	public typealias ValueKeyPath = ReferenceWritableKeyPath<EnclosingType, Value?>
-//	public typealias SelfKeyPath = ReferenceWritableKeyPath<EnclosingType, Self>
-//
-//	private let keyPath: ValueKeyPath
-//	private let value: Value?
-//
-//	public static subscript(_enclosingInstance instance: EnclosingType,
-//					 wrapped wrappedKeyPath: ValueKeyPath,
-//					 storage storageKeyPath: SelfKeyPath) -> Value? {
-//		get {
-//			return instance[keyPath: wrappedKeyPath]
-//		}
-//		set {
-//			let keyPath = instance[keyPath: storageKeyPath].keyPath
-//			let ancestorValue = instance[keyPath: keyPath]
-//			if ancestorValue == nil {
-//				let wrappedValue = instance[keyPath: wrappedKeyPath]
-//				if ancestorValue != wrappedValue {
-//					instance[keyPath: keyPath] = wrappedValue
-//				}
-//			}
-//			instance[keyPath: wrappedKeyPath] = newValue
-//		}
-//	}
-//
-//	@available(*, unavailable, message: "@CloudKitValue can only be applied to classes")
-//	public var wrappedValue: Value? {
-//		get { fatalError() }
-//		set { fatalError() }
-//	}
-//
-//	public init(_ keyPath: ValueKeyPath) {
-//		self.keyPath = keyPath
-//		self.value = nil
-//	}
-//
-//}
-
 @propertyWrapper
 public struct CloudKitValue<Value> where Value: Equatable, Value: Codable {
-	private enum CodingKeys: String, CodingKey {
-		case value
-		case ancestorValue
+	
+	public private(set) var clientValue: Value?
+	public var ancestorValue: Value? {
+		return _ancestorValue ?? clientValue
 	}
-
-	private var value: Value?
-	private var ancestorValue: Value?
-
+	
 	public var wrappedValue: Value? {
-		get { return value }
+		get { return clientValue }
 		set {
-			if ancestorValue == nil && value != newValue {
-				ancestorValue = value
+			if _ancestorValue == nil && clientValue != newValue {
+				_ancestorValue = clientValue
 			}
-			value = newValue
+			clientValue = newValue
 		}
 	}
 	
+	private enum CodingKeys: String, CodingKey {
+		case clientValue
+		case ancestorValue
+	}
+
+	private var _ancestorValue: Value?
+	
 	public init() {}
+	
 }
 
 extension CloudKitValue: Codable {
 
 	public init(from decoder: Decoder) throws {
 		if let value = try? Value.init(from: decoder) {
-			self.value = value
+			self.clientValue = value
 			return
 		}
 		
 		let container = try decoder.container(keyedBy: CodingKeys.self)
-		self.value = try container.decode(Value.self, forKey: .value)
-		self.ancestorValue = try container.decode(Value.self, forKey: .ancestorValue)
+		self.clientValue = try? container.decode(Value.self, forKey: .clientValue)
+		self._ancestorValue = try? container.decode(Value.self, forKey: .ancestorValue)
 	}
 
 	public func encode(to encoder: Encoder) throws {
 		var container = encoder.container(keyedBy: CodingKeys.self)
-		try container.encode(value, forKey: .value)
-		try container.encode(ancestorValue, forKey: .ancestorValue)
+		try container.encode(clientValue, forKey: .clientValue)
+		try container.encode(_ancestorValue, forKey: .ancestorValue)
 	}
 
 }
